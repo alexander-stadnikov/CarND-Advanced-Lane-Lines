c9c9561c45cca4571a1e94e0b6569529a70e8707
Alexander <alexander.stadnikov@gmail.com>
2020-12-30 02:21:01 +0100

    README: Calibration and Undistortion

--- project.py
+++ project.py
@@ -2,14 +2,19 @@ import glob
 import cv2 as cv
 from pipeline import Camera
 
+import matplotlib.pyplot as plt
+
 camera = Camera("./debug_output/calibration.cache")
 if not camera.calibrated:
     camera.calibrate_with_chessboard(glob.glob("./camera_cal/*.jpg"), pattern_size=(9, 6), output="debug_output")
 
-img = cv.imread("./camera_cal/calibration1.jpg")
+img = cv.imread("./test_images/test4.jpg")
 undistorted_img = camera.undistort(img)
-cv.imshow("Distorted", img)
-cv.waitKey(0)
-cv.imshow("Undistorted", undistorted_img)
-cv.waitKey(0)
-cv.destroyAllWindows()
+
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+ax1.imshow(img)
+ax1.set_title('Original Image', fontsize=50)
+ax2.imshow(undistorted_img)
+ax2.set_title('Undistorted Image', fontsize=50)
+plt.show()
